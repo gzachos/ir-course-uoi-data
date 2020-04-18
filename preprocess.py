a34a49f1cd7e103d9b0e9595d27344769116631b
George Z. Zachos <gzachos@cse.uoi.gr>
2020-04-18 11:47:31 +0300

    Implemented preprocessing using multiple processes
    Python's global interpreter lock (GIL) always allows exactly one thread
    to execute at a time, even if run on a multi-core processor. That's why
    I used multiprocessing than multithreading.

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 from bs4 import BeautifulSoup
+import multiprocessing
 
 
 ########################
@@ -17,20 +18,24 @@ def perror(*args, **kwargs):
     sys.stderr.flush()
 
 
-def print_stats(preproc_time, article_count):
+def print_stats(preproc_time):
     parse_fail_num = len(parse_failures)
     write_fail_num = len(write_failures)
     failed_num = parse_fail_num + write_fail_num
-    success_num = article_count - failed_num
+    success_num = total_article_count - failed_num
     print('\n############################## STATS ##############################')
     print('Extracted text from %d HTML files in %.3f minutes' %
-            (article_count - parse_fail_num, preproc_time / 60))
-    print('Failed to parse %d HTML documents [%.2f%%]' %
-            (parse_fail_num, parse_fail_num / article_count * 100))
-    print('Failed to write %d TXT documents [%.2f%%]' %
-            (write_fail_num, write_fail_num / article_count * 100))
-    print('Succesfully extracted text from %d documents [%.2f%%]' %
-            (success_num, success_num / article_count * 100))
+            (total_article_count - parse_fail_num, preproc_time / 60))
+    print('Preprocessing was performed using %d processes' % (num_processes))
+    if parse_fail_num != 0:
+        print('Failed to parse %d HTML documents [%.2f%%]' %
+                (parse_fail_num, parse_fail_num / total_article_count * 100))
+    if write_fail_num != 0:
+        print('Failed to write %d TXT documents [%.2f%%]' %
+                (write_fail_num, write_fail_num / total_article_count * 100))
+    if total_article_count != 0:
+        print('Succesfully extracted text from %d documents [%.2f%%]' %
+                (success_num, success_num / total_article_count * 100))
     print('###################################################################\n')
 
 
@@ -210,43 +215,96 @@ def parse_article(html_filename):
             # print(string)
         # Append misc sections like infobox/vcard etc. at the end.
         plain_text = dict(plain_text, **misc)
-        return plain_text, canonical_url
+        return (plain_text, canonical_url)
     except:
         perror('Cannot parse file: \'%s\'' % (html_filename))
         parse_failures.append(html_filename)
-        return {}, canonical_url
+        return ({}, canonical_url)
 
 
-def preprocess_files():
+def preprocess_files(html_files, pid, queue):
+    global total_article_count
     article_count = 0
+    for hf in html_files:
+        article_count += 1
+        #if article_count == 100:
+        #    break
+        #if 'User_space' in hf:
+        #if article_count == 11:
+        if True:
+            print('Process %2d: file: %4d - %s' % (pid, article_count, hf))
+            dictionary, url = parse_article(hf)
+            if dictionary != {} and url != None:
+                # print_plain_text(dictionary)
+                write_plain_text(dictionary, hf[:-5] + corpus_doc_suffix, url)
+
+    # Update total_article_count using synchronization to avoid race conditions
+    # Send the number of files processed to main thread
+    queue.put(article_count)
+    # print('Process %3d is exiting...' % (pid))
+
+
+def calculate_chunk(html_files, pid, num_processes):
+    num_files = len(html_files)
+    chunksize = num_files // num_processes
+    remainder = num_files % num_processes
+
+    if remainder != 0:   # hrefs cannot be divided in equal chunks
+        if chunksize == 0:   # num_files < num_processes
+            chunksize = 1
+            lb = pid
+            if pid >= num_files:   # No work to be assigned 
+                return (-1, -1)
+        else:   # num_files > num_processes
+            # First remainder processes get one more href than the remaining
+            if pid < remainder:
+                chunksize += 1
+                lb = pid * chunksize
+            else:
+                lb = remainder * (chunksize + 1) + (pid - remainder) * chunksize
+    else:   # Chunksize is the same for all processes
+        lb = pid * chunksize
+    return (lb, lb + chunksize)
+
+
+def multiprocess_preprocessing(html_files):
+    global total_article_count
+    process_list = []
+    # Create processes
+    queue = multiprocessing.Queue()
+    for i in range(num_processes):
+        lb, ub = calculate_chunk(html_files, i, num_processes)
+        if lb == -1 and ub == -1: # No work to be assigned
+            continue
+        arg_list = (html_files[lb:ub], i, queue)
+        process = multiprocessing.Process(target=preprocess_files, args=arg_list)
+        process_list.append(process)
+        process.start()
+    # Join processes
+    for process in process_list:
+        # Get the number of files processed by each thread
+        total_article_count += queue.get()
+        process.join()
+
+
+def list_html_files():
     try:
         files = os.listdir(repo_path)
         html_files = [f for f in files if f.endswith('.html')]
-        for hf in html_files:
-            article_count += 1
-            #if article_count == 100:
-            #    break
-            #if 'User_space' in hf:
-            #if article_count == 11:
-            if True:
-                print('------------------------------------------------------')
-                print(str(article_count) + ': ' + hf)
-                dictionary, url = parse_article(hf)
-                if dictionary != {} and url != None:
-                    # print_plain_text(dictionary)
-                    write_plain_text(dictionary, hf[:-5] + corpus_doc_suffix, url)
-        return article_count
+        return html_files
     except Exception as e:
         perror('Cannot list files in directory: %s' % (repo_path))
         exit(1)
 
+
 def main():
+    html_files = list_html_files()
     t0 = time.time()
-    article_count = preprocess_files()
+    multiprocess_preprocessing(html_files)
     t1 = time.time()
     preproc_time = t1 - t0
     print_failures()
-    print_stats(preproc_time, article_count)
+    print_stats(preproc_time)
 
 
 ###############
@@ -258,6 +316,9 @@ corpus_doc_suffix = '.txt'
 parse_failures = []   # filenames of HTML files that text wasn't extracted
 write_failures = []   # filenames of TXT files that couldn't be stored to disk
 field_separator = '\n\n'
+num_processors = os.cpu_count()
+num_processes = num_processors # Number of processes used during preprocessing
+total_article_count = 0   # How many articles where preprocessed by all processes
 
 
 if __name__ == '__main__':
