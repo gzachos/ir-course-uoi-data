35c841e6621ed3143f2fa63a02ca4b27dfef14b2
George Z. Zachos <gzachos@cse.uoi.gr>
2020-04-22 23:15:36 +0300

    Reimplemented HTML parser from scratch

--- preprocess.py
+++ preprocess.py
@@ -3,8 +3,9 @@
 import os
 import sys
 import time
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Comment
 import multiprocessing
+import traceback
 
 
 ########################
@@ -66,6 +67,10 @@ def print_plain_text(dictionary):
         print(dictionary[key])
 
 
+# Write plain text to a virtual XML file. The format is named virtual
+# because the output is not a valid XML but XML tags are only used as
+# field separators. Only one XML tag can exist per line, without any
+# other text.
 def write_virtual_xml(dictionary, target_filename, canonical_url):
     try:
         filepath = corpus_path + target_filename
@@ -92,6 +97,7 @@ def write_virtual_xml(dictionary, target_filename, canonical_url):
         outfile.write('</document>\n')
     except:
         perror('\tCannot write \'%s\'' % (filepath))
+        traceback.print_exc()
         write_failures.append(target_filename)
         remove_file(filepath)
 
@@ -111,6 +117,7 @@ def write_plain_text(dictionary, target_filename, canonical_url):
             outfile.write(cleanup_section(dictionary[key]) + '\n')
     except:
         perror('\tCannot write \'%s\'' % (filepath))
+        traceback.print_exc()
         write_failures.append(target_filename)
         remove_file(filepath)
 
@@ -132,61 +139,41 @@ def cleanup_section(string):
     return string.strip()
 
 
-def parse_h1(h):
-    hstr = ''
-    for c in h.children:
-        if c.string != None:
-            hstr += c.string
-    return hstr
-
-
-def parse_h(h):
-    for c in h.children:
-        if c.has_attr('class') and 'mw-headline' in c.attrs['class']:
-            if c.string != None:
-                return c.string
-            for sc in c.children:
-                if sc.string != None:
-                    return sc.string
-                # print('h: ' + str(sc.name) + " '" + str(sc.string) + "' " + str(sc.string == None))
+# Retrieve alt text from images inside 'mwe-math-element's.
+def get_img_alt_text(img):
+    if img == None:
+        return ''
+    if img.has_attr('class'):
+        img_classes = img.attrs['class']
+        if find_in(img_classes, 'mwe-math-fallback-image',
+                search_type='startswith'):
+            if img.has_attr('alt'):
+                string = img.attrs['alt']
+                # TODO parse math?
+                return string
     return ''
 
 
-def parse_html(content):
-    string = ''
-    # print('\n########################\n')
-    if content.string != None:
-        if content.name in ['sup', 'style', 'caption']: # caption is article title
-            return ''
-        # print(content.string)
-        return content.string
-    if content.name == None:
-        return ''
-
-    for c in content.children:
-        string += ' ' + parse_html(c)
-    '''
-    print(content.name)
-    print('\nText:\n')
-    print(content.get_text())
-    print('\nString:\n')
-    print(content.string)
-    print('\nRaw:\n')
-    print(content)
-    '''
+# Parse sup element while ignoring those that contain hrefs.
+def parse_sup(c, level, in_infobox):
+    string = parse_childrenof(c, level, ignore_hrefs=True, in_infobox=in_infobox)
+    if string != '':
+        string  = '^' + string
     return string
 
 
-def add_to_misc(c, misc, key, join_str):
-    string = parse_html(c)
+# Add string to corrsponding misc[key] using join_str to concatenate.
+def add_to_misc(key, string, join_str):
+    global misc
     if key in misc:
         misc[key] += join_str + string
     else:
         misc[key] = string
-    return string
 
 
-def search(classes, string, search_type='matches'):
+# Search classes and return True if string exists.
+# search_type = {'matches', 'contains', 'startswith'}
+def find_in(classes, string, search_type='matches'):
     for cls in classes:
         if (search_type == 'startswith' and cls.startswith(string)) or \
                 (search_type == 'contains' and string in cls) or \
@@ -195,75 +182,149 @@ def search(classes, string, search_type='matches'):
     return False
 
 
+# Used to call parse_child() recursively.
+def parse_childrenof(c, level, ignore_hrefs=False, in_infobox=False):
+    string = ''
+    for inner_c in c.children:
+        string += parse_child(inner_c, level+1, ignore_hrefs, in_infobox)
+    return string
+
+
+# Recursively parse current element/node. level corresponds to
+# the relative nesting level of the HTML tag, ignore_refs is
+# used to implement <sup> parsing and in_infobox is used to
+# parse HTML tags of infobox class.
+def parse_child(c, level, ignore_hrefs=False, in_infobox=False):
+    global curr_heading
+    ########################################################
+    # Segment A - Handle Comments and NavigableStrings     #
+    ########################################################
+    if isinstance(c, Comment):  # Subclass of NavigableString so keep first
+        return ''
+    if isinstance(c, NavigableString):
+        return c.string
+    ########################################################
+    # Segment B - Handle everything that is ignored        #
+    ########################################################
+    if c.name == 'style':
+        return ''
+    if c.name == 'script':  # Never true due to HTML subset
+        return ''
+    if c.name == 'caption':
+        return ''
+    if c.name == 'a' and ignore_hrefs == True:
+        return ''
+    if c.has_attr('role'):
+        roles = c.attrs['role']
+        if 'note' in roles:
+            return ''
+        if 'presentation' in roles:
+            return ''
+        if 'navigation' in roles:
+            return ''
+    if c.has_attr('class'):
+        classes = c.attrs['class']
+        if find_in(classes, 'navbox', search_type='contains'):
+            return ''
+        if find_in(classes, 'noprint'):
+            return ''
+        if find_in(classes, 'haudio'):
+            return ''
+        if find_in(classes, 'mw-editsection'):
+            return ''
+        if find_in(classes, 'mw-cite-backlink'):
+            return '' # ^ in reflist
+        if c.name == 'div':
+            if find_in(classes, 'toc'):
+                return ''
+    ########################################################
+    # Segment C - Handle elements containing useful text   #
+    ########################################################
+    if c.name == 'h2':
+        curr_heading = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+        plain_text[curr_heading] = ''
+        return ''
+    if c.name in ['h3','h4','h5','h6']:
+        string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+        plain_text[curr_heading] += string
+        return string
+    if c.name == 'blockquote':
+        string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+        add_to_misc('__quotes__', string, '\n')
+        plain_text[curr_heading] += string
+        return ''
+    if c.name == 'tr':  # Put one table row per line
+        string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+        string = string.replace('\n', ' ')
+        return string
+    if c.name == 'th' and in_infobox == True:
+        string = parse_childrenof(c, level, ignore_hrefs, in_infobox=True)
+        return ' ' + string + ' '  # Add the missing spaces
+    if c.has_attr('class'):
+        classes = c.attrs['class']
+        if c.name == 'div':
+            if find_in(classes, 'quotebox'):
+                string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+                add_to_misc('__quotes__', string, '\n')
+                return ''
+        if find_in(classes, 'thumbcaption'):
+            string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+            add_to_misc('__multimedia__', string, '\n')
+            return ''
+        if find_in(classes, 'gallerytext'):
+            string = parse_childrenof(c, level, ignore_hrefs, in_infobox)
+            add_to_misc('__multimedia__', string, '\n')
+            return ''
+        if find_in(classes, 'infobox'):
+            string = parse_childrenof(c, level, ignore_hrefs, in_infobox=True)
+            add_to_misc('__infobox__', string, '\n')
+            return ''
+        if find_in(classes, 'mwe-math-element'):  # Math formulas
+            img = c.find('img')
+            return get_img_alt_text(img)
+        if c.name == 'sup':  # Keep after check for noprint etc.
+            if find_in(classes, 'reference'):
+                return ''
+            elif find_in(classes, 'plainlinks'):
+                return ''
+            else:
+                return parse_sup(c, level, in_infobox)
+        if c.name == 'sub':
+            return '_' + parse_childrenof(c, level, ignore_hrefs, in_infobox)
+        if c.name == 'table':
+            if find_in(classes, 'clade'):  # Ignore cladograms!
+                return ''
+    else:  # Following elements have no classes
+        if c.name == 'sup':  # Keep after check for noprint etc.
+            return parse_sup(c, level, in_infobox)
+        if c.name == 'sub':
+            return '_' + parse_childrenof(c, level, ignore_hrefs, in_infobox)
+    return parse_childrenof(c, level, ignore_hrefs, in_infobox)
+
+
 # Returns dictionary of the form {heading: content} and the canonical url
 def parse_article(html_filename):
+    global plain_text, misc, curr_heading
     plain_text = {}
     misc = {}
     try:
         infile = open(repo_path + html_filename, mode='r', encoding='utf-8')
         soup = BeautifulSoup(infile, 'html5lib')
-        # soup = BeautifulSoup(infile, 'html.parser')
         canonical_url = soup.head.find('link', rel='canonical').get('href')
-        title = parse_h1(soup.body.find('h1', id='firstHeading'))
-        # print(title)
+        title = parse_childrenof(soup.body.find('h1', id='firstHeading'), level=0)
         content = soup.find('div', id='mw-content-text').contents[0]
-        # print(content.prettify())
         curr_heading = title
         plain_text[curr_heading] = ''
         for c in content.children:
-            # print('\n########################\n')
-            # print(c)
-            # print('\n')
-            if c.name == None:   # Ignore comments
-                continue
-            elif c.name == 'h2':
-                curr_heading = parse_h(c)
-                plain_text[curr_heading] = ''
-                continue
-            elif c.name in ['h3','h4','h5','h6']:
-                plain_text[curr_heading] += parse_h(c)
-                continue
-            # print(c.attrs)
-            elif c.has_attr('class'):
-                classes = c.attrs['class']
-                if search(classes, 'hatnote') == True or \
-                        search(classes, 'noprint') == True or \
-                        search(classes, 'haudio') == True:
-                    continue
-                if search(classes, 'navbox', 'contains') == True:
-                    continue
-                if search(classes, 'infobox') == True: # 'vcard'
-                    add_to_misc(c, misc, '__infobox__', '')
-                    continue
-                if search(classes, 'gallery') == True:
-                        add_to_misc(c, misc, '__multimedia__', '\n')
-                        continue
-                if c.name == 'table':
-                    if search(classes, 'box', 'startswith') == True:
-                        continue
-                if c.name == 'div':
-                    if search(classes, 'toc', 'contains') == True:
-                        continue
-                    if search(classes, 'thumb') == True:
-                        add_to_misc(c, misc, '__multimedia__', '\n')
-                        continue
-                    if search(classes, 'quotebox') == True:
-                        add_to_misc(c, misc, '__quotes__', '\n')
-                        continue
-                if c.name == 'blockquote':
-                    string = add_to_misc(c, misc, '__quotes__', '\n')
-                    plain_text[curr_heading] += string
-                    continue
-            string = parse_html(c)
-            plain_text[curr_heading] += string
-            #print(string)
+            plain_text[curr_heading] += parse_child(c, level = 0)
         # Append misc sections like infobox/vcard etc. at the end.
         plain_text = dict(plain_text, **misc)
         return (plain_text, canonical_url)
     except:
         perror('Cannot parse file: \'%s\'' % (html_filename))
+        traceback.print_exc()
         parse_failures.append(html_filename)
-        return ({}, canonical_url)
+        return ({}, None)
 
 
 def preprocess_files(html_files, pid, queue):
@@ -271,15 +332,12 @@ def preprocess_files(html_files, pid, queue):
     article_count = 0
     for hf in html_files:
         article_count += 1
-        #if article_count == 100:
-        #    break
-        #if 'Anseriformes' in hf:
-        #if article_count == 11:
+        #if '%22Hello,_World!%22_program' in hf:
         if True:
             print('Process %2d: file: %4d - %s' % (pid, article_count, hf))
             dictionary, url = parse_article(hf)
             if dictionary != {} and url != None:
-                # print_plain_text(dictionary)
+                #print_plain_text(dictionary)
                 write_plain_text(dictionary, hf[:-5] + corpus_doc_suffix, url)
                 # write_virtual_xml(dictionary, hf[:-5] + corpus_doc_suffix_xml, url)
 
@@ -294,20 +352,20 @@ def calculate_chunk(html_files, pid, num_processes):
     chunksize = num_files // num_processes
     remainder = num_files % num_processes
 
-    if remainder != 0:   # hrefs cannot be divided in equal chunks
-        if chunksize == 0:   # num_files < num_processes
+    if remainder != 0:  # hrefs cannot be divided in equal chunks
+        if chunksize == 0:  # num_files < num_processes
             chunksize = 1
             lb = pid
-            if pid >= num_files:   # No work to be assigned 
+            if pid >= num_files:  # No work to be assigned
                 return (-1, -1)
-        else:   # num_files > num_processes
+        else:  # num_files > num_processes
             # First remainder processes get one more href than the remaining
             if pid < remainder:
                 chunksize += 1
                 lb = pid * chunksize
             else:
                 lb = remainder * (chunksize + 1) + (pid - remainder) * chunksize
-    else:   # Chunksize is the same for all processes
+    else:  # Chunksize is the same for all processes
         lb = pid * chunksize
     return (lb, lb + chunksize)
 
@@ -339,6 +397,7 @@ def list_html_files():
         return html_files
     except Exception as e:
         perror('Cannot list files in directory: %s' % (repo_path))
+        traceback.print_exc()
         exit(1)
 
 
@@ -355,17 +414,16 @@ def main():
 ###############
 # Global data #
 ###############
-repo_path = './repository/'   # Where downloaded HTML files are stored
-corpus_path = './corpus/'   # Where corpus (parsed) text files will be stored
+repo_path = './repository/'  # Where downloaded HTML files are stored
+corpus_path = './corpus/'  # Where corpus (parsed) text files will be stored
 corpus_doc_suffix = '.txt'
 corpus_doc_suffix_xml = '.xml'
-parse_failures = []   # filenames of HTML files that text wasn't extracted
-write_failures = []   # filenames of TXT files that couldn't be stored to disk
+parse_failures = []  # filenames of HTML files that text wasn't extracted
+write_failures = []  # filenames of TXT files that couldn't be stored to disk
 field_separator = '\n\n'
 num_processors = os.cpu_count()
-# Number of processes used during preprocessing
-num_processes = (num_processors - 1) if num_processors > 1 else 1
-total_article_count = 0   # How many articles where preprocessed by all processes
+num_processes = num_processors # Number of processes used during preprocessing
+total_article_count = 0  # How many articles where preprocessed by all processes
 
 
 if __name__ == '__main__':
