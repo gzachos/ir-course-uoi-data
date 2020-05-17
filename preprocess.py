5ef43e71a1b0afdbc3e6697cffd8d91b29955d87
George Z. Zachos <gzachos@cse.uoi.gr>
2020-05-17 17:12:45 +0300

    Added date modified/published extraction from articles
    To support range queries in Lucene

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,9 @@ import time
 from bs4 import BeautifulSoup, NavigableString, Comment
 import multiprocessing
 import traceback
-
+import datetime
+import pytz
+import json
 
 ########################
 # Function definitions #
@@ -127,7 +129,8 @@ def get_summary(section_text):
 # because the output is not a valid XML but XML tags are only used as
 # field separators. Only one XML tag can exist per line, without any
 # other text.
-def write_virtual_xml(dictionary, target_filename, canonical_url):
+def write_virtual_xml(dictionary, target_filename, canonical_url,
+        date_modified, date_published):
     try:
         filepath = corpus_path + target_filename
         outfile = open(filepath, mode='w', encoding='utf-8')
@@ -135,6 +138,14 @@ def write_virtual_xml(dictionary, target_filename, canonical_url):
         outfile.write('<url>\n')
         outfile.write(canonical_url)
         outfile.write('\n</url>\n')
+        if date_published != "":
+            outfile.write('<published>\n')
+            outfile.write(date_published)
+            outfile.write('\n</published>\n')
+        if date_modified != "":
+            outfile.write('<updated>\n')
+            outfile.write(date_modified)
+            outfile.write('\n</updated>\n')
         first_key = True
         for key in dictionary:
             if first_key == True:
@@ -382,6 +393,31 @@ def parse_child(c, level, ignore_hrefs=False, in_infobox=False):
     return parse_childrenof(c, level, ignore_hrefs, in_infobox)
 
 
+def get_article_dates(soup):
+    string = soup.find_all("script")[-2].text
+    try:
+        json_data = json.loads(string)
+    except:
+        perror(title)
+        return "", ""
+    date_modified = ""
+    if 'dateModified' in json_data:
+        date_modified = to_epoch_utc(json_data['dateModified'])
+    date_published = ""
+    if 'datePublished' in json_data:
+        date_published = to_epoch_utc(json_data['datePublished'])
+    return date_modified, date_published
+
+
+def to_epoch_utc(date_str):
+    dt_obj = datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+    utc_tz = pytz.timezone('UTC')
+    dt_obj = utc_tz.localize(dt_obj)
+    timestamp = int(dt_obj.timestamp())
+    # print(datetime.datetime.utcfromtimestamp(timestamp))
+    return str(timestamp)
+
+
 # Returns dictionary of the form {heading: content} and the canonical url
 def parse_article(html_filename):
     global plain_text, misc, curr_heading, read_summary, title
@@ -391,6 +427,7 @@ def parse_article(html_filename):
     try:
         infile = open(repo_path + html_filename, mode='r', encoding='utf-8')
         soup = BeautifulSoup(infile, 'html5lib')
+        date_modified, date_published = get_article_dates(soup)
         canonical_url = soup.head.find('link', rel='canonical').get('href')
         title = parse_childrenof(soup.body.find('h1', id='firstHeading'), level=0)
         content = soup.find('div', id='mw-content-text').contents[0]
@@ -403,7 +440,7 @@ def parse_article(html_filename):
             add_to_misc('__summary__', NO_DESC_AVAIL, '')
         # Append misc sections like infobox/vcard etc. at the end.
         plain_text = dict(plain_text, **misc)
-        return (plain_text, canonical_url)
+        return (plain_text, canonical_url, date_modified, date_published)
     except:
         perror('Cannot parse file: \'%s\'' % (html_filename))
         traceback.print_exc()
@@ -416,11 +453,12 @@ def preprocess_files(html_files, pid, queue):
     for hf in html_files:
         article_count += 1
         print('Process %2d: file: %4d - %s' % (pid, article_count, hf))
-        dictionary, url = parse_article(hf)
+        dictionary, url, date_modified, date_published = parse_article(hf)
         if dictionary != {} and url != None:
             #print_plain_text(dictionary)
             #write_plain_text(dictionary, hf[:-5] + corpus_doc_suffix, url)
-            write_virtual_xml(dictionary, hf[:-5] + corpus_doc_suffix_xml, url)
+            write_virtual_xml(dictionary, hf[:-5] + corpus_doc_suffix_xml, url,
+                    date_modified, date_published)
 
     # Send to main thread the number of files processed and the
     # filenames of the HTML files that couldn't be parsed or written.
